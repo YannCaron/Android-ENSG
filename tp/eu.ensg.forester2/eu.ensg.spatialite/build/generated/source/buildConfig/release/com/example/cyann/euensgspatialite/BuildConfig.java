4eda9e2f92d2df8f4f9ce6898f0a0431c5947bc9
Yann Caron <cyann74@gmail.com>
2016-01-25 22:58:18 +0100

    spatialite installation

---
+++ tp/eu.ensg.forester2/eu.ensg.spatialite/build/generated/source/buildConfig/release/com/example/cyann/euensgspatialite/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.cyann.euensgspatialite;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.cyann.euensgspatialite";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
